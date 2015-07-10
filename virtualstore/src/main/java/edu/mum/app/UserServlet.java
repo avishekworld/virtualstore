4eb96f7cd924748180df3e83cddd2b7ad13e797a
avishek.ahmed@gmail.com <avishek.ahmed@gmail.com>
2015-07-10 14:33:17 -0500

    upload

--- virtualstore/src/main/java/edu/mum/app/UserServlet.java
+++ virtualstore/src/main/java/edu/mum/app/UserServlet.java
@@ -32,6 +32,8 @@ public class UserServlet extends HttpServlet {
 	    
 	    request.setAttribute("user", u);
 		request.getRequestDispatcher("index.jsp").forward(request, response);
+		
+		//life
 	}
 
 }
